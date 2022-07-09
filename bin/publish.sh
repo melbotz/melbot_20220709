4ba7a037b315a3780a23fe205adc867db78ccc37
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-09 19:11:10 +0200

    update birth

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,4 +8,5 @@ RELAY=$(git config gitmark.relay)
 GENESIS="gitmark:d60703354709c88724f058cc2184a28831f6c8635324837650f5ccfff3ce5595:0"
 CREATED=1657316029
 
+
 git-mark-list $(git config gitmark.secret) --genesis "${GENESIS}" --created "${CREATED}" -g $(cat package.json | jq -r '.repository.url')  | websocat $RELAY
