9f9b096ddb07b87b1bab799ae0e5d54f68014277
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-09 18:46:45 +0200

    add committments to nostr

--- bin/publish.sh
+++ bin/publish.sh
@@ -5,5 +5,7 @@ npm publish
 
 #RELAY="ws://nostr.rocks:1617"
 RELAY=$(git config gitmark.relay)
+GENESIS="gitmark:d60703354709c88724f058cc2184a28831f6c8635324837650f5ccfff3ce5595:0"
+CREATED=1657316029
 
-git-mark-list $(git config gitmark.secret) -g $(cat package.json | jq -r '.repository.url')  | websocat $RELAY
+git-mark-list $(git config gitmark.secret) --genesis "${GENESIS}" --created "${CREATED}" -g $(cat package.json | jq -r '.repository.url')  | websocat $RELAY
