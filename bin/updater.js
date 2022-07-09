722aae8c80d993c5c192c7e56a1e8153ffda2227
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-09 19:51:18 +0200

    add ping to updater

--- bin/updater.js
+++ bin/updater.js
@@ -25,9 +25,31 @@ setInterval(loop, TIME)
 
 if (wsdeploy) {
   try {
+
+
+
     var relay = 'ws://nostr.rocks:1617'
     console.log('relay', relay)
     const ws = new WebSocket(relay)
+
+    function heartbeat() {
+      console.log('ping')
+      clearTimeout(this.pingTimeout)
+
+      // Use `WebSocket#terminate()`, which immediately destroys the connection,
+      // instead of `WebSocket#close()`, which waits for the close timer.
+      // Delay should be equal to the interval at which your server
+      // sends out pings plus a conservative assumption of the latency.
+      this.pingTimeout = setTimeout(() => {
+        this.terminate()
+      }, 30000 + 1000)
+    }
+
+    ws.on('open', heartbeat)
+    ws.on('ping', heartbeat)
+    ws.on('close', function clear() {
+      clearTimeout(this.pingTimeout)
+    })
 
     ws.on('open', function open() {
       console.log('opened', relay)
