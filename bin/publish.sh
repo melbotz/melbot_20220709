60b1ff32bd99a2000620174e314e5496554ea405
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-09 14:08:46 +0200

    tag repo in nostr

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-git-mark-list $(git config gitmark.secret) | websocat $(git config gitmark.relay)
+#RELAY="ws://nostr.rocks:1617"
+RELAY=$(git config gitmark.relay)
+
+git-mark-list $(git config gitmark.secret) -g $(cat package.json | jq -r '.repository.url')  | websocat $RELAY
